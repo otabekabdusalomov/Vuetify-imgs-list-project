b0b729ff8ec5157467419e8291f40a4b63da0080
otabekabdusalomov <otabekabdusalamov2001@gmail.com>
2023-01-03 00:32:18 +0300

    navbar section

--- src/components/NavbarSection.vue
+++ src/components/NavbarSection.vue
@@ -1,6 +1,31 @@
 <template>
     <v-app-bar app color="#3853D8" dark>
-      
+        <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+        <v-divider vertical class="ml-1 mr-3"></v-divider>
+        <v-btn icon>
+            <v-icon>mdi-menu</v-icon>
+        </v-btn>
+        
+        <v-toolbar-title>
+            <span class="caption">Select</span><br>Category
+        </v-toolbar-title>
+        <v-divider vertical class="ml-5"></v-divider>
+        <div style="position:absolute; margin-left:auto; margin-right:auto; left: 0; right: 0; text-align: center;">
+            <h4>AAE IdeaPro</h4>
+        </div>
+
+        <v-spacer></v-spacer>
+        <v-divider vertical></v-divider>
+        <v-btn icon class="mx-1">
+            <v-icon>mdi-account-outline</v-icon>
+        </v-btn>
+
+        <v-divider vertical></v-divider>
+        <v-btn icon class="mx-1">
+            <v-badge color="#94D0EF" content="2"></v-badge>
+        </v-btn>
     </v-app-bar>
 </template>
 

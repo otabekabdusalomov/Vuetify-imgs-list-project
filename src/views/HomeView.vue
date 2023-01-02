30ee5cfc8fefe4b74837a10738c9618bcd484f41
otabekabdusalomov <otabekabdusalamov2001@gmail.com>
2023-01-03 00:01:52 +0300

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <NavbarSection class="index" />
+    <DashboardCard />
+    <FooterSection />
+  </v-app>
+</template>
+
+<script>
+import NavbarSection from '../components/NavbarSection.vue'
+import DashboardCard from '../components/DashboardCard.vue'
+import FooterSection from '../components/FooterSection.vue'
+import { defineComponent } from 'vue';
+// import Dashboard from '@/components/Dashboard.vue';
+
+// Components
+// import HelloWorld from '../components/HelloWorld.vue';
+
+export default defineComponent({
+  // name: 'HomeView',
+  components: {
+    // HelloWorld,
+    NavbarSection,
+    DashboardCard,
+    FooterSection,
+  },
+});
+</script>
+
+
+<style>
+.index {
+  z-index: 9999;
+}
+</style>
